6ebe3a0c6d289dabbf3689cee9ca7fdc94e36585
apleek3 <apleek3@gmail.com>
2018-07-11 18:34:56 -0700

    ajax call now working. working on stopping the gifs

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,15 +5,15 @@ $(document).ready(function () {
 
     function makeButtons() {
 
-        $("#astronomy").empty();
+        $("#astronomyButtons").empty();
 
         // Looping through the array of astronomical bodies
         for (var i = 0; i < astronomicalBodies.length; i++) {
             var a = $("<button>"); // generates buttons for each astrobody in the array
-            a.addClass(""); // Adds a class   
+            a.addClass("button"); // Adds a class   
             a.attr("data-name", astronomicalBodies[i]);// Adding a data-attribute with a value of the movie at index i
             a.text(astronomicalBodies[i]); // Providing the button's text with a value of the movie at index i
-            $("#astronomy").append(a); // Adding the button to the HTML
+            $("#astronomyButtons").append(a); // Adding the button to the HTML
         }
     }
 
@@ -26,7 +26,54 @@ $(document).ready(function () {
 
     makeButtons();
 
-    $("#astronomy").on("click", function () {
+    $(document).on("click", ".button", function () {
+        
+        // Grabbing and storing the data-astronomy property value from the button
+        var astronomy = $(this).attr("data-name");
+
+        // Constructing a queryURL using the astronomy name
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+            astronomy + "&api_key=dzRd4JQJhi8oMRInE0JuiDG5R4zD2Ls4";
+
+        // Performing an AJAX request with the queryURL
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        })
+            // After data comes back from the request
+            .then(function (response) {
+                console.log(queryURL);
+
+                console.log(response);
+                // storing the data from the AJAX request in the results variable
+                var results = response.data;
+
+                // Looping through each result item
+                for (var i = 0; i < results.length; i++) {
+
+                    // Creating and storing a div tag
+                    var astronomyDiv = $("<div>");
+
+                    // Creating a paragraph tag with the result item's rating
+                    var p = $("<p>").text("Rating: " + results[i].rating);
+
+                    // Creating and storing an image tag
+                    var astronomyImage = $("<img>");
+                    // Setting the src attribute of the image to a property pulled off the result item
+                    astronomyImage.attr("src", results[i].images.fixed_height.url);
+                    astronomyImage.addClass("astronomyGif");
+
+                    // Appending the paragraph and image tag to the astronomyDiv
+                    astronomyDiv.append(p);
+                    astronomyDiv.append(astronomyImage);
+
+                    // Prependng the astronomyDiv to the HTML page in the "#gifs-appear-here" div
+                    $("#gifs").prepend(astronomyDiv);
+                }
+            });
+    });
+
+    $(document).on("click", ".astronomyGif", function () {
         var state = $(this).attr("data-state");        // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
         if (state === "still") {        // If the clicked image's state is still, update its src attribute to what its data-animate value is.
             $(this).attr("src", $(this).attr("data-animate"));
